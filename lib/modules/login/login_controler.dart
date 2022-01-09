2c32656cbbf98e90e80e779ed513b5ecc11f5436
EdgarXongas <edgaroboe@outlook.com>
2022-01-08 22:13:31 -0300

    first commit

---
+++ lib/modules/login/login_controler.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/shared/auth/auth_controler.dart';
+import 'package:payflow/shared/models/user_models.dart';
+
+class LoginControler {
+  final authControler = AuthControler();
+
+  Future<void> googleSignIn(BuildContext context) async {
+    GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: ['email'],
+    );
+    try {
+      final response = await _googleSignIn.signIn();
+      final user =
+          UserModel(name: response!.displayName!, photoUrl: response.photoUrl);
+
+      authControler.setUser(context, user);
+      print(response);
+    } catch (error) {
+      authControler.setUser(context, null);
+      print(error);
+    }
+  }
+}
