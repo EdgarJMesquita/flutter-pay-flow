d3e049fe3a472a40edda83d73d8568f48ac36a5c
Edgar <edgaroboe@outlook.com>
2022-01-23 16:15:42 -0300

    t1

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _AppFirebaseState extends State<AppFirebase> {
         future: _initialization,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            return Material(
+            return const Material(
               child: Center(
                 child: Text(
                   'Não foi possível inicializar o firebase',
